6fb2f481f19e8450598a7cd0e323c2f610a84c2d
ihab-taysir <tihab215@gmail.com>
2025-08-13 14:29:35 +0300

    solve inngest bug

--- config/inngest.js
+++ config/inngest.js
@@ -1,25 +1,23 @@
 import { Inngest } from "inngest";
-import connectDB from "./db";
-import User from "@/models/user";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "quickcart-next" });
 
 // Inngest Function to save user data to a database
 export const syncUserCreation = inngest.createFunction(
-  {
-    id: "sync-user-from-clerk",
-  },
-
+  { id: "sync-user-from-clerk" },
   { event: "clerk/user.created" },
-
   async ({ event }) => {
+    // Import DB and Model dynamically to avoid build-time serialization issues
+    const connectDB = (await import("./db")).default;
+    const User = (await import("@/models/user")).default;
+
     const { id, first_name, last_name, email_addresses, image_url } =
       event.data;
 
     const userData = {
       _id: id,
-      name: first_name + " " + last_name,
+      name: `${first_name} ${last_name}`,
       email: email_addresses[0].email_address,
       imageURL: image_url,
     };
@@ -29,21 +27,20 @@ export const syncUserCreation = inngest.createFunction(
   }
 );
 
-// Inngest Function to save user data to a database
+// Inngest Function to update user data in the database
 export const syncUserUpdation = inngest.createFunction(
-  {
-    id: "update-user-from-clerk",
-  },
-
+  { id: "update-user-from-clerk" },
   { event: "clerk/user.updated" },
-
   async ({ event }) => {
+    const connectDB = (await import("./db")).default;
+    const User = (await import("@/models/user")).default;
+
     const { id, first_name, last_name, email_addresses, image_url } =
       event.data;
 
     const userData = {
       _id: id,
-      name: first_name + " " + last_name,
+      name: `${first_name} ${last_name}`,
       email: email_addresses[0].email_address,
       imageURL: image_url,
     };
@@ -53,15 +50,14 @@ export const syncUserUpdation = inngest.createFunction(
   }
 );
 
-// Inngest Function to delete user data from a database
+// Inngest Function to delete user data from the database
 export const syncUserDeletion = inngest.createFunction(
-  {
-    id: "delete-user-with-clerk",
-  },
-
+  { id: "delete-user-with-clerk" },
   { event: "clerk/user.deleted" },
-
   async ({ event }) => {
+    const connectDB = (await import("./db")).default;
+    const User = (await import("@/models/user")).default;
+
     const { id } = event.data;
 
     await connectDB();
